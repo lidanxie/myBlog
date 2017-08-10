124b751a937c572c5f6e565ecc03bb3f28726f56
lidan.xie <lidan.xie@vipshop.com>
2017-08-10 19:08:27 +0800

    修改

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,16 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
+    <input type="text" v-model="myself">
+    {{show()}}
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
@@ -25,7 +16,16 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      myself: ''
+    }
+  },
+  created () {
+    console.log(this.myself)
+  },
+  methods: {
+    show () {
+      console.log(this.myself)
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Vuex from './vuex'
 
 Vue.config.productionTip = false
 
