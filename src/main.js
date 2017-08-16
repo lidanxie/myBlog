0b3e4b5d7d15c84a27dd4ea539eba109841396ea
lidanxie <987905457@qq.com>
2017-08-16 19:30:20 +0800

    修改路由方式

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
+import routerMap from './router/router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
+Vue.use(VueRouter)
+const router = new VueRouter({ routes: routerMap })
+
+
 new Vue({
   el: '#app',
   router,
---
+++ src/router/router.js
@@ -0,0 +1,9 @@
+import index from '../components/index'
+
+export default [
+	{
+	  path: '/index',
+	  name: 'index',
+	  component: index
+	}
+]
